4fcc425082dc43c6c8575dbef584021cfcadcc41
MinsungKim <alstjd8826@naver.com>
2020-12-30 13:49:39 +0900

    final commit!!

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
-
-canvas.width = 700;
-canvas.height = 700;
-
 
 let painting = false;
 let filling = false;
@@ -42,6 +47,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 // input 값이 변경될때 작동하는 함수 : 굵기 변경
@@ -62,11 +68,36 @@ function handleModeClick(event){
     }
 }
 
+function handleCanvasCanvas(event){
+    // console.log(event);
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    } 
+}
+
+//우클릭 기능 없애는 함수
+function handleCM(event){
+    // console.log(event);
+    event.preventDefault();
+}
+
+//이미지 저장 함수
+function saveImage(event){
+    // console.log(event);
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting); /* 마우스 클릭할 때*/
     canvas.addEventListener("mouseup", startPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasCanvas);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -78,4 +109,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(save){
+    save.addEventListener("click", saveImage);
 }
